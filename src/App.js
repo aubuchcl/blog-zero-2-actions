de8a6f275af12190c84164b92b87dccea43b0edf
Chris Aubuchon <aubuchcl@gmail.com>
2023-11-15 15:35:37 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Here is a message.</p>
+        <p>Ive been updated.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Hello from React!
+          Hello from React, and Cycle!
         </a>
       </header>
     </div>
