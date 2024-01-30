07651fa52bb8b0f2a2067be8ecadea880a75d2ee
Chris Aubuchon <aubuchcl@gmail.com>
2024-01-30 15:15:36 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Starting Point </p>
+        <p>Look its changed.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Hello everyone
         </a>
